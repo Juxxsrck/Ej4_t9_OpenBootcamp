db1ad5bc3fc0a298ffc8e85812b413a38d343387
juxxsrck <justine.pulido@gmail.com>
2022-08-23 22:00:08 -0500

    Se crea nueva clase Trebajador que herada de  la clase Persona, y se imprime los datos "nombre, edad, telefono y (credito/sueldo)" en pantalla

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@ public class Main {
     public static void main(String[] args) {
         Cliente cliente = new Cliente(30, 32132342, "Justine Pulido", 10500);
         System.out.println("El cliente es: "  + cliente.getNombre());
-        System.out.println("La edad de " + cliente.getNombre() + " es de: " + cliente.getEdad());
+        System.out.println("La edad de " + cliente.getNombre() + " es de: " + cliente.getEdad() + " años.");
         System.out.println("El telefono de " + cliente.getNombre() + " es: " + cliente.getTelefono());
         System.out.println("El credito de " + cliente.getNombre() + " es de: " + cliente.getCredito() + " euros.");
+
+        System.out.println();
+        System.out.println();
+
+        Trabajador trabajador = new Trabajador(21, 31509873, "James Barreto", 2000);
+        System.out.println("El trabajador es: " + trabajador.getNombre());
+        System.out.println("La edad de " + trabajador.getNombre() + " es de: " + trabajador.getEdad() + " años.");
+        System.out.println("El telefono de " + trabajador.getNombre() + " es: " + trabajador.getTelefono());
+        System.out.println("El sueldo de " + trabajador.getNombre() + " es de: " + trabajador.getSueldo() + " euros.");
     }
+
 }
--- src/Trabajador.java
+++ src/Trabajador.java
@@ -1,2 +1,17 @@
-public class Trabajador {
+public class Trabajador extends Persona{
+
+    private int sueldo;
+
+    public Trabajador(int edad, int telefono, String nombre, int sueldo) {
+        super(edad, telefono, nombre);
+        this.sueldo = sueldo;
+    }
+
+    public int getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(int sueldo) {
+        this.sueldo = sueldo;
+    }
 }
